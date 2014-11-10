b8835a56cf358c32489407cc72ff294b0eb283e4
Tushar Tuteja <ttuteja@gmail.com>
2014-11-10 23:20:20 +0530

    Initial Commit

---
+++ django/mysite/mysite/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+
+urlpatterns=[url(r'^polls/',include('polls.urls', namespace = "polls")),
+url(r'^admin/',include(admin.site.urls)),]
